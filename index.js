ce0e16bd7d48f7127a4614ffc2fb4463ffaaa0aa
Benni-beep <krikelkr4kel@gmail.com>
2021-03-04 16:31:04 +0100

    formatting + breathing cycle length works now

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 "use strict"
 
+/**
+ * To Do
+ * from second round onwards the first number of the breathing cycles is displayed once to little
+ * run on shorter intervals so button press "seve retetion time" has no perceived delay
+ */
+
 // const startBreathingCyclesButton = document.getElementById("start-breathing-cycles");
 const breathingCycleCounterDisplay = document.getElementById("breathing-counter-display");
 const retentionTimeDisplay = document.getElementById("retention-time-display");
@@ -7,53 +13,67 @@ const saveRetentionTimeButton = document.getElementById("save-retention-time");
 const retentionTimesLog = document.getElementById("retention-times-log");
 
 const render = (props) => {
-        switch (props.state) {
-            case 'breathing': {
-                retentionTimesLog.setAttribute("style", "display: none");
-                breathingCycleCounterDisplay.textContent = props.breathingCycleCounter;
-                break;
-            }
-            case 'retention': {
-                breathingCycleCounterDisplay.setAttribute("style", "display: none;");
-                retentionTimeDisplay.setAttribute("style", "display: block;");
-                saveRetentionTimeButton.setAttribute("style", "display: block;");
+    const integerPart = (float) => Math.trunc(float);
+    const minutes = (float) => integerPart(float / 60);
+    const seconds = (float) => integerPart(float) - minutes(float) * 60;
+    const displayFormat = (n) => n.toString().padStart(2, 0);
 
-                retentionTimeDisplay.textContent = Math.trunc((Date.now() - props.retentionTime) / 1000);
-                break;
-            }
-            case 'holdBreath': {
-                retentionTimeDisplay.setAttribute("style", "display: none;");
-                saveRetentionTimeButton.setAttribute("style", "display: none;");
-                retentionTimesLog.setAttribute("style", "display: block");
-                retentionTimesLog.innerHTML = "";
 
-                props.retentionLog.forEach(logEntry => {
-                    const entry = document.createElement("li");
-                    const content = document.createTextNode(logEntry);
-                    entry.appendChild(content);
-                    retentionTimesLog.appendChild(entry)
-                })
-
-                breathingCycleCounterDisplay.setAttribute("style", "display: block");
-                breathingCycleCounterDisplay.textContent = props.holdBreathCounter;
-                break;
-            }
-            default: {
-                throw new Error('Should never happen');
-            }
+    switch (props.state) {
+        case 'breathing': {
+            breathingCycleCounterDisplay.textContent = props.breathingCycleCounter;
+            break;
         }
+        case 'retention': {
+            breathingCycleCounterDisplay.setAttribute("style", "display: none;");
+            retentionTimeDisplay.setAttribute("style", "display: block;");
+            saveRetentionTimeButton.setAttribute("style", "display: block;");
+
+            // retentionTimeDisplay.textContent = Math.trunc((Date.now() - props.retentionTime) / 1000);
+
+            let elapsedTime = Math.trunc((Date.now() - props.retentionTime) / 1000);
+            const elapsedMinutes = displayFormat(minutes(elapsedTime));
+            const elapsedSeconds = displayFormat(seconds(elapsedTime));
+
+            retentionTimeDisplay.textContent = `${elapsedMinutes}:${elapsedSeconds}`
+            break;
+        }
+        case 'holdBreath': {
+            retentionTimeDisplay.setAttribute("style", "display: none;");
+            saveRetentionTimeButton.setAttribute("style", "display: none;");
+            retentionTimesLog.setAttribute("style", "display: block");
+            retentionTimesLog.innerHTML = "";
+
+            props.retentionLog.forEach(logEntry => {
+                const entry = document.createElement("li");                              
+                const minutesToLog = displayFormat(minutes(logEntry));
+                const secondsToLog = displayFormat(seconds(logEntry));                
+                const content = document.createTextNode(`${minutesToLog}:${secondsToLog}`)
+                entry.appendChild(content);
+                retentionTimesLog.appendChild(entry)
+            })
+
+            breathingCycleCounterDisplay.setAttribute("style", "display: block");
+            breathingCycleCounterDisplay.textContent = props.holdBreathCounter;
+            break;
+        }
+        default: {
+            throw new Error('Should never happen');
+        }
+    }
 }
 
 const main = () => {
     // config
     // these values shouldn't really change, in fact, they could be supplied
     // via `main` parameters
-    const breathingCycleLength = 3;
-    const breathingCycles = 3;
+    const breathingCycleLength = 4;
+    const breathingCycles = 30;
 
     // state
     // these values constantly change as part of `tick` and hooks
     let breathingCycleCounter = breathingCycles;
+    let breathingCycleLengthCounter = 0;
     let holdBreathCounter = 15;
     let retentionTime = 0;
     const retentionLog = [];
@@ -70,7 +90,7 @@ const main = () => {
 
     // state change hooks
     // these can run before / after every state change
-    function preHoldBreath () {
+    function preHoldBreath() {
         retentionLog.push((Date.now() - retentionTime) / 1000);
         retentionTime = 0;
         state = 'holdBreath';
@@ -85,7 +105,7 @@ const main = () => {
             retentionLog,
             holdBreathCounter,
         };
-        console.log(_state)
+        console.log(_state);
         render(_state);
         switch (state) {
             case 'breathing': {
@@ -93,8 +113,13 @@ const main = () => {
                     breathingCycleCounter = breathingCycles;
                     retentionTime = Date.now();
                     state = 'retention';
-                };                
-                breathingCycleCounter--;
+                };
+                // give the same number {breathingCycleLength} times, then --
+                breathingCycleLengthCounter++
+                if (breathingCycleLengthCounter === breathingCycleLength) {
+                    breathingCycleCounter--
+                    breathingCycleLengthCounter = 0;
+                }
                 break;
             }
             case 'retention': {
